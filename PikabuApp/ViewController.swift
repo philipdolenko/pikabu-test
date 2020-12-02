29d17b91b5a08553b907976b93a5a17a2df4e3c9
Philip Dolenko <philipdolenko@gmail.com>
2020-12-02 14:05:25 +0200

    Default project

---
+++ PikabuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PikabuApp
+//
+//  Created by Philip Dolenko on 02.12.2020.
+//  Copyright © 2020 Philip Dolenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
